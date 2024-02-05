3f39dc343feeb799b9ae0f5364306caaf946219c
MOUFAOUID <moufaouidmouhcine@gmail.com>
2024-02-05 10:20:59 +0100

    add result to console

---
+++ index.js
@@ -0,0 +1,16 @@
+function addition(a, b){
+    console.log("The result is: " ,a + b)
+}
+
+const subtraction = function(a, b){
+    console.log(a - b)
+}
+
+const multiplication = (a, b) => console.log(a * b);
+
+const division = (a, b) => console.log(a / b);
+
+addition(1, 3)
+subtraction(4, 2)
+multiplication(2, 5)
+division(4, 2)
